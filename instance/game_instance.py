3a6d1ad4cfef87b0fe8802df51f312668b2aeb10
北斗余晖 <81358657+MikhailTapio@users.noreply.github.com>
2025-11-04 05:27:22 +1100

    :tada: Fix wrong default path for generating shortcuts

--- instance/game_instance.py
+++ instance/game_instance.py
@@ -26,6 +26,19 @@
 #
 #  You should have received a copy of the GNU Affero General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import hashlib
 import json
@@ -36,9 +49,6 @@ from typing import Dict, Optional, List, Tuple  # (新增 Tuple)
 
 import win32api
 
-
-# 注意：这个模块需要 'pywin32'
-# 请运行: pip install pywin32
 
 def _calculate_sha256(filepath: Path) -> Optional[str]:
     """计算文件的 SHA256 哈希值"""
--- ui/dialogs.py
+++ ui/dialogs.py
@@ -52,6 +52,19 @@
 #
 #  You should have received a copy of the GNU Affero General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import tkinter as tk
 from tkinter import ttk, messagebox, filedialog  # (已修改)
@@ -313,7 +326,9 @@ class AutoUpdateConfigDialog(BaseDialog):
         self.preset_id = preset_id
 
         # --- 获取默认保存路径 ---
-        desktop = Path(os.path.join(os.environ["USERPROFILE"], "Desktop"))
+        shell = win32com.client.Dispatch('WScript.Shell')
+        desktop_path_str = shell.SpecialFolders('Desktop')
+        desktop = Path(desktop_path_str)
         default_name = f"{_('lki.autoupdate.shortcut_default_title') % self.instance_name}.lnk"
         self.shortcut_path_var = tk.StringVar(value=str(desktop / default_name))
         self.start_game_var = tk.BooleanVar(value=True)
